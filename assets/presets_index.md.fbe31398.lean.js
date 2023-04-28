a3d908283a2be3bfc8dda3373dbba4d062151458
Alfred-Skyblue <yangpanteng@gmail.com>
2023-04-28 02:16:15 +0000

    Deploying to gh-pages from @ Alfred-Skyblue/unocss-docs-cn@1694fac29c0a5634662d7ca16423f52524e9e636 🚀

---
+++ assets/presets_index.md.fbe31398.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as r,c as s,R as e}from"./chunks/framework.a0edb2bb.js";const m=JSON.parse('{"title":"官方包","description":"","frontmatter":{},"headers":[],"relativePath":"presets/index.md","lastUpdated":1682648134000}'),a={name:"presets/index.md"},d=e("",7),o=[d];function n(c,i,h,u,f,p){return r(),s("div",null,o)}const b=t(a,[["render",n]]);export{m as __pageData,b as default};
